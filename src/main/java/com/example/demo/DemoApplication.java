6ecdf4dd25f050f832e3dac6e59fd17dce1588c2
harshal.patil <harshal5@amnex.com>
2025-06-13 08:31:59 +0530

    updated a code snippet

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -18,7 +18,7 @@ public class DemoApplication {
 
 	@GetMapping("path")
 	public String getMethodName() {
-		return "Hello World! Oracle deployment via github runner." ;
+		return "Hello World! Oracle deployment via github runner. JHKJDAHflkjs" ;
 	}
 	
 
