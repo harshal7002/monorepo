ac21aa00e7b93074f8472d683f85d28cd0d35143
harshal.patil <harshal5@amnex.com>
2025-06-30 11:58:17 +0530

    Added abc

---
+++ oracle-deployment_v1/deploy_to_weblogic.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Set WebLogic environment variables
+export WL_HOME="/home/harshal/weblogic/weblogic"  # Path to WebLogic installation
+#export JAVA_HOME="/opt/oracle/jdk-11"  # Path to Oracle JDK
+#export PATH="$JAVA_HOME/bin:$WL_HOME/bin:$PATH"
+
+# Set WebLogic credentials and other variables
+export WLS_USERNAME="weblogic"
+export WLS_PASSWORD="Weblogic@123"
+export WLS_URL="t3://localhost:7001"
+export WAR_FILE_PATH="target/demo-0.0.1-SNAPSHOT.war"
+
+# Use Jython for WebLogic Scripting Tool (WLST)
+$WL_HOME/oracle_common/common/bin/wlst.sh << EOF
+from weblogic.management.configuration import *
+try:
+    # Connect to the WebLogic Server
+    connect('$WLS_USERNAME', '$WLS_PASSWORD', '$WLS_URL')
+
+    # Start a session and check for the server's availability
+    edit()
+    startEdit()
+
+    # Deploy the WAR file
+    deploy(appName='SpringBootApp', path='$WAR_FILE_PATH', targets='AdminServer', stageMode='stage')
+
+    # Save and activate the changes
+    save()
+    activate()
+
+    print("Deployment successful!")
+
+except Exception as e:
+    print("Deployment failed:", e)
+    undo()
+    cancel()
+
+# Exit WLST
+exit()
+EOF
---
+++ oracle-deployment_v1/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("Started successfully hello world!!");
+	}
+
+	@GetMapping("path")
+	public String getMethodName() {
+		return "Hello World! Oracle deployment via github runner. Integrated Sonarqube Successfully!!!~" ;
+	}
+	
+
+}
