31fa93cc00ec076dd203c17a62b809945bcf8773
harshal.patil <harshal5@amnex.com>
2025-07-06 13:47:10 +0530

    updated main.yml to fix multiple deployment

--- SOA/oracle-deployment_v1/src/main/java/com/example/demo/DemoApplication.java
+++ SOA/oracle-deployment_v1/src/main/java/com/example/demo/DemoApplication.java
@@ -18,7 +18,7 @@ public class DemoApplication {
 
 	@GetMapping("path")
 	public String getMethodName() {
-		return "Hello World! Oracle deployment via github runner. Instance 2" ;
+		return "Hello World! Oracle deployment via github runner in i2" ;
 	}
 	
 
